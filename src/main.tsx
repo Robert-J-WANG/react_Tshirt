0f539be94d312335df687792b6d2a0e7c8a0e81a
Robert-J-WANG <robin.nz420@gmail.com>
2023-10-28 15:00:34 +1300

    update1

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import { SharedStateProvider } from "./context/UseCartContainer.js";
 import { App } from "./App";
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <SharedStateProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </SharedStateProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
